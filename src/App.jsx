e2dddd6dcfc9fb188bedf1c15fe337d3ae283b69
martin_chakarov <martin.d.chakarov@gmail.com>
2022-11-06 19:54:36 +0200

    Create landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Blob from './components/Blob'
 
 export default function App() {
   
   return (
-    <main className="app">
-    </main>
+    <div className="app">
+      <div className="background">
+        <Blob/>
+        <Blob/>
+      </div>
+      <div className="start-quiz">
+        <h1 className="title">Quizzical</h1>
+        <p className="description">Test your knowledge with some random trivia questions!</p>
+        <button className="start-btn">Start quiz</button>
+      </div>
+    </div>
   );
 }
